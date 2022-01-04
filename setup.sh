e1b75af5a2d11549a7446cd8cb0d9f1ce5aa7e11
Bo Yao <bo@near.org>
2022-01-04 10:04:28 +0800

    add platform independant wasi-sdk in setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,17 @@ rm -rf vendor
 mkdir vendor
 cd vendor
 # wasi-sdk
-wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-linux.tar.gz
-tar xvf wasi-sdk-11.0-linux.tar.gz
+SYSTEM=$(uname)
+if [ "${SYSTEM}" = "Linux" ]; then
+    system=linux
+elif [ "${SYSTEM}" = "Darwin" ]; then
+    system=macos
+else
+    echo "Unsupported system ${SYSTEM}"
+    exit 1
+fi
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-11/wasi-sdk-11.0-"${system}".tar.gz
+tar xvf wasi-sdk-11.0-"${system}".tar.gz
 # binaryen
 git clone --branch wasi-stub --single-branch https://github.com/near/binaryen
 cd binaryen
