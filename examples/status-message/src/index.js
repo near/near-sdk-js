53f66109b35afdce27d63e39f929b4276c4bba8d
Bo Yao <bo@near.org>
2022-05-10 14:43:36 +0800

    add tests

---
+++ examples/status-message/src/index.js
@@ -0,0 +1,23 @@
+import {NearContract, NearBindgen, call, view, near} from '../../../sdk'
+
+@NearBindgen
+class StatusMessage extends NearContract {
+    constructor() {
+        super()
+        this.records = {}
+    }
+
+    @call
+    set_status(message) {
+        let account_id = near.signerAccountId()
+        env.log(`${account_id} set_status with message ${message}`)
+        this.records[account_id] = message
+    }
+
+    @view
+    get_status(account_id) {
+        env.log(`get_status for account_id ${account_id}`)
+        return this.records[account_id]
+    }
+}
+
