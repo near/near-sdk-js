91ebbb29c1d7986356efe104fae83fb95888b8e0
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-05-16 19:59:40 +0300

    sdk files moved to srdc folder

--- sdk/index.js
+++
@@ -1,5 +0,0 @@
-import {call, view, NearBindgen} from './near-bindgen'
-import { NearContract } from './near-contract'
-import * as near from './api'
-
-export {call, view, NearBindgen, NearContract, near}
---
+++ sdk/src/index.js
@@ -0,0 +1,19 @@
+import {
+    call,
+    view,
+    NearBindgen
+} from './near-bindgen'
+
+import {
+    NearContract
+} from './near-contract'
+
+import * as near from './api'
+
+export {
+    call,
+    view,
+    NearBindgen,
+    NearContract,
+    near,
+}
