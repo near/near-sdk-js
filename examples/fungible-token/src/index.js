3c4171d3dbe205012524d9e86320d146b626a7a1
Bo Yao <bo@near.org>
2022-05-16 16:39:33 +0800

    fungible token example works

--- examples/fungible-token/src/index.js
+++ examples/fungible-token/src/index.js
@@ -1,7 +1,5 @@
-import { ReceiptOutcome } from 'near-workspaces'
 import {NearContract, NearBindgen, call, view, near} from '../../../sdk'
 import {LookupMap} from '../../../sdk/collections/lookup-map'
-import * as near from '../../../sdk/api'
 
 function assert(b, str)
 {
@@ -14,27 +12,33 @@ function assert(b, str)
 
 @NearBindgen
 class FungibleToken extends NearContract {
-    constructor(prefix) {
+    constructor(prefix, totalSupply) {
         super()
         this.accounts = new LookupMap(prefix)
-        this.totalSupply = '0'
-        // doesn't need accountStorageUsage as rust as in JS contract, storage cost management is automatic
+        this.totalSupply = totalSupply
+        this.accounts.set(near.signerAccountId(), totalSupply)
+        // don't need accountStorageUsage like rust in JS contract, storage deposit management is automatic in JSVM
+    }
+
+    deserialize() {
+        super.deserialize()
+        this.accounts = Object.assign(new LookupMap, this.accounts)
     }
 
     internalDeposit(accountId, amount) {
-        let balance = this.accounts[accountId] || '0'
+        let balance = this.accounts.get(accountId) || '0'
         let newBalance = BigInt(balance) + BigInt(amount)
-        this.accounts[accountId] = newBalance.toString()
+        this.accounts.set(accountId, newBalance.toString())
         this.totalSupply = (BigInt(this.totalSupply) + BigInt(amount)).toString()
     }
 
     internalWithdraw(accountId, amount) {
-        let balance = this.accounts[accountId] || '0'
+        let balance = this.accounts.get(accountId) || '0'
         let newBalance = BigInt(balance) - BigInt(amount)
-        assert(newBalance < 0n, "The account doesn't have enough balance")
-        this.accounts[accountId] = newBalance.toString()
+        assert(newBalance >= 0n, "The account doesn't have enough balance")
+        this.accounts.set(accountId, newBalance.toString())
         let newSupply = BigInt(this.totalSupply) - BigInt(amount)
-        assert(newSupply < 0n, "Total supply overflow")
+        assert(newSupply >= 0n, "Total supply overflow")
         this.totalSupply = newSupply.toString()
     }
 
@@ -56,7 +60,19 @@ class FungibleToken extends NearContract {
     ftTransferCall(receiverId, amount, memo, msg) {
         let senderId = near.predecessorAccountId()
         this.internalTransfer(senderId, receiverId, amount, memo)
-        return near.jsvmCall(receiverId, 'ftOnTransfer', [senderId, amount, msg, receiverId])
-        // TODO: call ftResolveTransfer on the return, if it's defined (in a child class of FungibleToken)
+        let onTransferRet = near.jsvmCall(receiverId, 'ftOnTransfer', [senderId, amount, msg, receiverId])
+        // In JS, do not need a callback, ftResolveTransfer after ftOnTransfer Returns
+        // If any logic after ftOnTransfer Returns is required, just do it on onTransferRet.
+        return onTransferRet
+    }
+
+    @view
+    ftTotalSupply() {
+        return this.totalSupply
+    }
+
+    @view
+    ftBalanceOf(accountId) {
+        return this.accounts.get(accountId) || '0'
     }
 }
