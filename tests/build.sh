7771f44de185a79bcdbea056e84ec1e817082509
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-06-02 14:59:48 +0300

    dict test contract and test added

--- tests/build.sh
+++ tests/build.sh
@@ -6,3 +6,4 @@ near-sdk build src/vector.js build/vector.base64
 near-sdk build src/lookup-map.js build/lookup-map.base64
 near-sdk build src/lookup-set.js build/lookup-set.base64
 near-sdk build src/unordered-set.js build/unordered-set.base64
+near-sdk build src/function-params.js build/function-params.base64
---
+++ tests/src/function-params.js
@@ -0,0 +1,33 @@
+import {
+    NearContract,
+    NearBindgen,
+    call,
+    view,
+    near,
+} from 'near-sdk-js'
+
+/**
+ * Simple contract to test function parameters
+ */
+@NearBindgen
+class FunctionParamsTestContract extends NearContract {
+    constructor() {
+        super()
+        this.val1 = 'default1';
+        this.val2 = 'default2';
+        this.val3 = 'default3';
+    }
+
+    @call
+    set_values(param1, param2, param3) {
+        near.log(JSON.stringify({ param1, param2, param3 }));
+        this.val1 = param1;
+        this.val2 = param2;
+        this.val3 = param3;
+    }
+
+    @view
+    get_values() {
+        return { val3: this.val3, val2: this.val2, val1: this.val1 }
+    }
+}
