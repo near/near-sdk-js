6b5232d8db5cd581bdc48a405b04db35584c5752
Bo Yao <bo@near.org>
2022-06-10 17:42:35 +0800

    fix certain bytes copy in jsvm.c and tests read write storage

--- tests/__tests__/bytes.ava.js
+++ tests/__tests__/bytes.ava.js
@@ -57,7 +57,7 @@ test('Log expected types work', async t => {
     );
 });
 
-test('Log unexpected types work', async t => {
+test('Log unexpected types not logging', async t => {
     const { jsvm, ali, testContract } = t.context.accounts;
 
     let r = await ali.callRaw(jsvm, 'call_js_contract', encodeCall(testContract.accountId, 'log_unexpected_input_tests', ''), { attachedDeposit: '100000000000000000000000' });
@@ -88,5 +88,85 @@ test('Log invalid utf-16 sequence panic', async t => {
     t.deepEqual(
         r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
         'String encoding is bad UTF-16 sequence.'
+    );
+});
+
+function encodeStateKey(k) {
+    return Buffer.concat([Buffer.from('test-contract.test.near/state/'), Buffer.from(k)]).toString('base64')
+}
+
+test('storage write bytes tests', async t => {
+    const { jsvm, ali, testContract } = t.context.accounts;
+
+    await ali.call(jsvm, 'call_js_contract', encodeCall(testContract.accountId, 'storage_write_bytes', ''), { attachedDeposit: '100000000000000000000000' });
+    let stateMap = new Map()
+    // viewState doesn't work, because it tries to convert key to utf-8 string, which is not
+    let state = await jsvm.viewStateRaw()
+    for (let {key, value} of state) {
+        stateMap.set(key, value)
+    }
+
+    t.deepEqual(
+        stateMap.get(encodeStateKey('abc')),
+        Buffer.from('def').toString('base64')
+    );
+    t.deepEqual(
+        stateMap.get(encodeStateKey([0x00, 0x01, 0xff])),
+        Buffer.from([0xe6, 0xb0, 0xb4]).toString('base64')
+    );
+    t.deepEqual(
+        stateMap.get(encodeStateKey([0xe6, 0xb0, 0xb4])),
+        Buffer.from([0x00, 'a'.charCodeAt(0), 'b'.charCodeAt(0)]).toString('base64')
+    );
+});
+
+test('storage write unexpected types tests', async t => {
+    const { jsvm, ali, testContract } = t.context.accounts;
+
+    await ali.call(jsvm, 'call_js_contract', encodeCall(testContract.accountId, 'storage_write_unexpected_input', ''), { attachedDeposit: '100000000000000000000000' });
+    let stateMap = new Map()
+    // viewState doesn't work, because it tries to convert key to utf-8 string, which is not
+    let state = await jsvm.viewStateRaw()
+    for (let {key, value} of state) {
+        stateMap.set(key, value)
+    }
+    console.log(await jsvm.viewState())
+
+    t.deepEqual(
+        stateMap.get(encodeStateKey('123')),
+        Buffer.from('456').toString('base64')
+    );
+    // pass in utf-8 string instead of bytes, key and value become empty 
+    t.deepEqual(
+        stateMap.get(encodeStateKey([0xe6, 0xb0, 0xb4])),
+        undefined
+    );
+    t.deepEqual(
+        stateMap.get(encodeStateKey([])),
+        ''
+    );
+});
+
+test('Storage read bytes tests', async t => {
+    const { jsvm, ali, testContract } = t.context.accounts;
+
+    await ali.call(jsvm, 'call_js_contract', encodeCall(testContract.accountId, 'storage_write_bytes', ''), { attachedDeposit: '100000000000000000000000' });
+    let r = await jsvm.viewRaw('view_js_contract', encodeCall(testContract.accountId, 'storage_read_ascii_bytes', ''))
+    console.log(r)
+    t.deepEqual(
+        r.result,
+        [100, 101, 102]
+    );
+
+    r = await jsvm.viewRaw('view_js_contract', encodeCall(testContract.accountId, 'storage_read_arbitrary_bytes_key_utf8_sequence_bytes_value', ''))
+    t.deepEqual(
+        r.result,
+        [0xe6, 0xb0, 0xb4]
+    );
+
+    r = await jsvm.viewRaw('view_js_contract', encodeCall(testContract.accountId, 'storage_read_utf8_sequence_bytes_key_arbitrary_bytes_value', ''))
+    t.deepEqual(
+        r.result,
+        [0x00, 'a'.charCodeAt(0), 'b'.charCodeAt(0)]
     );
 });
