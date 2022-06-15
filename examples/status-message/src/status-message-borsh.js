1c4343b421f7aa3f6198b922fd9f7f07c45abba4
Bo Yao <bo@near.org>
2022-06-15 15:48:14 +0800

    get borsh compiles, but cannot get schema right

--- examples/status-message/src/status-message-borsh.js
+++ examples/status-message/src/status-message-borsh.js
@@ -1,40 +1,65 @@
 import { NearContract, NearBindgen, call, view, near } from 'near-sdk-js'
 import { serialize, deserialize } from 'borsh';
-
-const schema = new Map();;
+import { panic } from '../../../src/api';
 
 @NearBindgen
 class StatusMessage extends NearContract {
     constructor() {
         super()
-        this.records = {}
+        this.records = new Map()
     }
 
-
     deserialize() {
-        let state = near.jsvmStorageRead('STATE');
-        if (state) {
-            Object.assign(this, deserialize(schema, state));
-        } else {
-            throw new Error('Contract state is empty')
-        }
+        borshDeserializeStatusMessage(this)
     }
 
     serialize() {
-        near.jsvmStorageWrite('STATE', serialize(schema, this));
+        borshSerializeStatusMessage(this)
     }
 
     @call
     set_status(message) {
         let account_id = near.signerAccountId()
         env.log(`${account_id} set_status with message ${message}`)
-        this.records[account_id] = message
+        this.records.set(account_id, message)
     }
 
     @view
     get_status(account_id) {
         env.log(`get_status for account_id ${account_id}`)
-        return this.records[account_id] || null
+        return this.records.get(account_id) || null
     }
 }
 
+// This doesn't work, maybe due to @Nearbindgen cause this no longer the same class as StatusMessage
+// const schema = new Map([[StatusMessage, {
+//     'kind': 'struct',
+//     'fields': [
+//         ['records', {kind: 'map', key: 'string', value: 'string'}]
+//     ]
+// }]]);
+
+class StatusMessageBorsh {}
+
+const schema = new Map([[StatusMessageBorsh, {
+    'kind': 'struct',
+    'fields': [
+        ['records', {kind: 'map', key: 'string', value: 'string'}]
+    ]
+}]]);
+
+function borshSerializeStatusMessage(statusMessage) {
+    let temp = new StatusMessageBorsh()
+    temp.records = statusMessage.records
+    near.jsvmStorageWrite('STATE', serialize(schema, temp));
+}
+
+function borshDeserializeStatusMessage(to) {
+    let state = near.jsvmStorageRead('STATE');
+
+    if (state) {
+        Object.assign(to, deserialize(schema, state));
+    } else {
+        throw new Error('Contract state is empty')
+    }
+}
