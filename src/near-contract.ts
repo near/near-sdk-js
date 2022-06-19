9ff902445df8cd3f2f10c7b2d23019d99888dfbc
austinabell <austinabell8@gmail.com>
2022-06-18 22:19:49 -0400

    make near contract types explicit any

--- src/near-contract.ts
+++ src/near-contract.ts
@@ -1,25 +1,25 @@
-import * as near from './api'
+import * as near from "./api";
 
 export class NearContract {
-    deserialize() {
-        let state = near.jsvmStorageRead('STATE');
-        if (state) {
-            Object.assign(this, JSON.parse(state))
-        } else {
-            throw new Error('Contract state is empty')
-        }
+  deserialize() {
+    let state = near.jsvmStorageRead("STATE");
+    if (state) {
+      Object.assign(this, JSON.parse(state));
+    } else {
+      throw new Error("Contract state is empty");
     }
+  }
 
-    serialize() {
-        near.jsvmStorageWrite('STATE', JSON.stringify(this));
-    }
+  serialize() {
+    near.jsvmStorageWrite("STATE", JSON.stringify(this));
+  }
 
-    static deserializeArgs() {
-        let args = near.jsvmArgs();
-        return JSON.parse(args || '{}')
-    }
+  static deserializeArgs(): any {
+    let args = near.jsvmArgs();
+    return JSON.parse(args || "{}");
+  }
 
-    static serializeReturn(ret) {
-        return JSON.stringify(ret)
-    }
+  static serializeReturn(ret: any) {
+    return JSON.stringify(ret);
+  }
 }
