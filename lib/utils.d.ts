594df6f2a9c43687b65a6f4a990502db9e7eaefc
austinabell <austinabell8@gmail.com>
2022-06-18 21:35:57 -0400

    update utils to ts

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,3 +1,3 @@
-export function u8ArrayToBytes(array: any): string;
-export function bytesToU8Array(bytes: any): Uint8Array;
-export function bytes(strOrU8Array: any): any;
+export declare function u8ArrayToBytes(array: Uint8Array): string;
+export declare function bytesToU8Array(bytes: any): Uint8Array;
+export declare function bytes(strOrU8Array: string | Uint8Array): string;
