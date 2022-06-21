789cf43756cb99d88c0da6babecd863850338ec6
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-06-21 23:17:51 +0300

    path fixed

--- cli/cli.js
+++ cli/cli.js
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 
+import fs from 'fs/promises'
+import path from 'path';
+import { exec as exec_ } from 'child_process';
+import { promisify } from 'util';
+
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { babel } from '@rollup/plugin-babel';
-
 import { rollup } from 'rollup';
-
-import { exec as exec_ } from 'child_process';
-import path from 'path';
-import { promisify } from 'util';
-
-const VENDOR_FOLDER = 'node_modules/near-sdk-js/vendor';
 
 const exec = promisify(exec_);
 
+const PROJECT_DIR = `../../../`;
+const NEAR_SDK_JS = 'node_modules/near-sdk-js';
+const VENDOR = `${NEAR_SDK_JS}/vendor`;
 const OS = await executeCommand('uname -s', true);
 const ARCH = await executeCommand('uname -m', true);
-const QJSC_DIR = `./node_modules/near-sdk-js/quickjs`;
-const QJSC_BUILDS_DIR = `./node_modules/near-sdk-js/cli/qjsc`;
+const QJSC_DIR = `${NEAR_SDK_JS}/quickjs`;
+const QJSC_BUILDS_DIR = `${NEAR_SDK_JS}/cli/qjsc`;
 const QJSC = `${QJSC_BUILDS_DIR}/${OS}-${ARCH}-qjsc`;
 
 yargs(hideBin(process.argv))
@@ -106,7 +107,7 @@ async function cratreHeaderFileWithQjsc(rollupTarget, qjscTarget) {
 // Enclaved build functions
 async function createEnclavedContract(qjscTarget, enclavedContractTarget) {
     console.log(`Saving enclaved bytecode to ${enclavedContractTarget}`);
-    const SAVE_BYTECODE_SCRIPT = './node_modules/near-sdk-js/cli/save_bytecode.js';
+    const SAVE_BYTECODE_SCRIPT = `${NEAR_SDK_JS}/cli/save_bytecode.js`;
     await executeCommand(`node ${SAVE_BYTECODE_SCRIPT} ${qjscTarget} ${enclavedContractTarget}`);
 }
 
@@ -118,24 +119,31 @@ async function cleanupEnclavedBuildArtifacts(qjscTraget) {
 // Standalone build functions
 async function wasiStubStandaloneContract(standaloneContractTarget) {
     console.log(`Excecuting wasi-stup...`);
-    const WASI_STUB = `${VENDOR_FOLDER}/binaryen/wasi-stub/run.sh`;
+    const WASI_STUB = `${VENDOR}/binaryen/wasi-stub/run.sh`;
     await executeCommand(`${WASI_STUB} ${standaloneContractTarget} >/dev/null`);
 }
 
 async function cleanupStandaloneBuildArtifacts() {
-    console.log(`Cleanup standalone build artifacts...`);
-    await executeCommand(`rm build/code.h build/methods.h build/builder.c`);
+    // console.log(`Cleanup standalone build artifacts...`); //TODO: cleanup files when names will be available
+    // await executeCommand(`rm build/code.h build/methods.h build/builder.c`);
 }
 
 async function createStandaloneMethodsHeaderFile(rollupTarget) {
     console.log(`Genereting methods.h file`);
-    const CODEGEN_SCRIPT = './node_modules/near-sdk-js/cli/builder/codegen.js';
-    await executeCommand(`node ${CODEGEN_SCRIPT} ${rollupTarget}`);
+    let source = rollupTarget;
+    const buildPath = path.dirname(source);
+    let mod = await import(`${PROJECT_DIR}/${rollupTarget}`);
+    let exportNames = Object.keys(mod);
+    let methods = '';
+    for (let name of exportNames) {
+        methods += `DEFINE_NEAR_METHOD(${name})\n`;
+    }
+    await fs.writeFile(`${buildPath}/methods.h`, methods); // TODO: should we usd unique file name here?
 }
 
 async function createStandaloneWasmContract(standaloneContractTarget) {
     console.log(`Creating ${standaloneContractTarget} contract...`);
-    const WASI_SDK_PATH = `${VENDOR_FOLDER}/wasi-sdk-11.0`;
+    const WASI_SDK_PATH = `${VENDOR}/wasi-sdk-11.0`;
 
     const CC = `${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot`
     let DEFS = `-D_GNU_SOURCE '-DCONFIG_VERSION="2021-03-27"' -DCONFIG_BIGNUM`
@@ -143,7 +151,7 @@ async function createStandaloneWasmContract(standaloneContractTarget) {
         DEFS = DEFS + ' -DNIGHTLY'
     }
     const INCLUDES = `-I${QJSC_DIR} -I.`
-    const ORIGINAL_BUILDER_PATH = './node_modules/near-sdk-js/cli/builder/builder.c';
+    const ORIGINAL_BUILDER_PATH = `${NEAR_SDK_JS}/cli/builder/builder.c`;
     const NEW_BUILDER_PATH = `${path.dirname(standaloneContractTarget)}/builder.c`
     const SOURCES = `${NEW_BUILDER_PATH} ${QJSC_DIR}/quickjs.c ${QJSC_DIR}/libregexp.c ${QJSC_DIR}/libunicode.c ${QJSC_DIR}/cutils.c ${QJSC_DIR}/quickjs-libc-min.c ${QJSC_DIR}/libbf.c`;
     const LIBS = `-lm`
