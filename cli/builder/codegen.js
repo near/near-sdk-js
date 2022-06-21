8d0152bf9b856fcc7a9c9274de2805d58b0a1a55
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-06-21 19:52:36 +0300

    codegen build path fixed

--- cli/builder/codegen.js
+++ cli/builder/codegen.js
@@ -3,13 +3,14 @@ import path from 'path'
 
 async function main() {
     let source = path.resolve(process.argv[process.argv.length-1])
+    const buildPath = path.dirname(source);
     let mod = await import(source)
     let exportNames = Object.keys(mod)
     let methods = ''
     for (let name of exportNames) {
         methods += `DEFINE_NEAR_METHOD(${name})\n`
     }
-    await fs.writeFile('methods.h', methods)
+    await fs.writeFile(`${buildPath}/methods.h`, methods) // TODO: should we usd unique file name here?
 }
 
 main()
