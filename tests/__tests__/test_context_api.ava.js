ca437e889005c90281708f35d3016a5be6e0926d
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-06-22 15:09:50 +0300

    test scripts improved

--- tests/__tests__/test_context_api.ava.js
+++ tests/__tests__/test_context_api.ava.js
@@ -10,9 +10,9 @@ test.before(async t => {
     const root = worker.rootAccount;
 
     // Deploy the test contract.
-    const testContract = await root.createAndDeploy(
+    const contextApiContract = await root.createAndDeploy(
         root.getSubAccount('test-contract').accountId,
-        'build/test_contract.wasm',
+        'build/context_api.wasm',
     );
 
     // Test users
@@ -20,7 +20,7 @@ test.before(async t => {
 
     // Save state for test runs
     t.context.worker = worker;
-    t.context.accounts = { root, testContract, ali };
+    t.context.accounts = { root, contextApiContract, ali };
 });
 
 test.after(async t => {
@@ -31,32 +31,32 @@ test.after(async t => {
 
 
 test('get current account id correct', async t => {
-    const { ali, testContract } = t.context.accounts;
-    let r = await ali.call(testContract, 'get_current_account_id', '');
-    t.is(r, testContract.accountId);
+    const { ali, contextApiContract } = t.context.accounts;
+    let r = await ali.call(contextApiContract, 'get_current_account_id', '');
+    t.is(r, contextApiContract.accountId);
 })
 
 test('get signer account id correct', async t => {
-    const { ali, testContract } = t.context.accounts;
-    let r = await ali.call(testContract, 'get_signer_account_id', '');
+    const { ali, contextApiContract } = t.context.accounts;
+    let r = await ali.call(contextApiContract, 'get_signer_account_id', '');
     t.is(r, ali.accountId);
 })
 
 test('get predecessor account id correct', async t => {
-    const { ali, testContract } = t.context.accounts;
-    let r = await ali.call(testContract, 'get_predecessor_account_id', '');
+    const { ali, contextApiContract } = t.context.accounts;
+    let r = await ali.call(contextApiContract, 'get_predecessor_account_id', '');
     t.is(r, ali.accountId);
 })
 
 test('get signer account pk correct', async t => {
-    const { ali, testContract } = t.context.accounts;
-    let r = await ali.callRaw(testContract, 'get_signer_account_pk', '');
+    const { ali, contextApiContract } = t.context.accounts;
+    let r = await ali.callRaw(contextApiContract, 'get_signer_account_pk', '');
     // the prefixing byte 0 indicates it's a ED25519 PublicKey, see how PublicKey is serialized in nearcore
     t.deepEqual(Buffer.from(r.result.status.SuccessValue, 'base64'), Buffer.concat([Buffer.from([0]), Buffer.from((await ali.getKey(ali.accountId)).getPublicKey().data)]));
 })
 
 test('get input correct', async t => {
-    const { ali, testContract } = t.context.accounts;
-    let r = await ali.callRaw(testContract, 'get_input', new Uint8Array([0, 1, 255]));
+    const { ali, contextApiContract } = t.context.accounts;
+    let r = await ali.callRaw(contextApiContract, 'get_input', new Uint8Array([0, 1, 255]));
     t.is(r.result.status.SuccessValue, Buffer.from(new Uint8Array([0, 1, 255])).toString('base64'));
 })
--- tests/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-## Run this script with `npm run build`
-
-near-sdk build src/test_contract.js build/test_contract.wasm
