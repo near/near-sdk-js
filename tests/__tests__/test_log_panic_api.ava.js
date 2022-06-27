29130e63760d569d81f4e87d5c27d5ba8b03b1b3
Bo Yao <bo@near.org>
2022-06-27 16:49:23 +0800

    add log and panic tests

---
+++ tests/__tests__/test_log_panic_api.ava.js
@@ -0,0 +1,136 @@
+import { Worker } from 'near-workspaces';
+import test from 'ava';
+
+
+test.before(async t => {
+    // Init the worker and start a Sandbox server
+    const worker = await Worker.init();
+
+    // Prepare sandbox for tests, create accounts, deploy contracts, etx.
+    const root = worker.rootAccount;
+
+    // Deploy the test contract.
+    const testContract = await root.createAndDeploy(
+        root.getSubAccount('test-contract').accountId,
+        'build/log_panic_api.wasm',
+    );
+
+    // Test users
+    const ali = await root.createSubAccount('ali');
+
+    // Save state for test runs
+    t.context.worker = worker;
+    t.context.accounts = { root, testContract, ali };
+});
+
+test.after(async t => {
+    await t.context.worker.tearDown().catch(error => {
+        console.log('Failed to tear down the worker:', error);
+    });
+});
+
+
+test('Log expected types work', async t => {
+    const { ali, testContract } = t.context.accounts;
+
+    let r = await ali.callRaw(testContract, 'log_expected_input_tests', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.logs,
+        [
+            "abc",
+            "水",
+            "333",
+            '\x00\x01\xff',
+            '\xe6\xb0\xb4',
+            "水",
+            "水"
+        ]
+    );
+});
+
+test('Log unexpected types not logging', async t => {
+    const { ali, testContract } = t.context.accounts;
+
+    let r = await ali.callRaw(testContract, 'log_unexpected_input_tests', '');
+    // logUtf8 and logUtf16 only works with bytes, trying to log it with string is unexpected and behavior is undefined
+    // in this specific case, it logs nothing
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.logs,
+        [
+            "",
+            "",
+        ]
+    );
+});
+
+test('Log invalid utf-8 sequence panic', async t => {
+    const { ali, testContract } = t.context.accounts;
+
+    let r = await ali.callRaw(testContract, 'log_invalid_utf8_sequence_test', '');
+    // console.log(JSON.stringify(r, null, 2))
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'String encoding is bad UTF-8 sequence.'
+    );
+});
+
+test('Log invalid utf-16 sequence panic', async t => {
+    const { ali, testContract } = t.context.accounts;
+
+    let r = await ali.callRaw(testContract, 'log_invalid_utf16_sequence_test', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'String encoding is bad UTF-16 sequence.'
+    );
+});
+
+test('panic tests', async t => {
+    const { ali, testContract } = t.context.accounts;
+    let r = await ali.callRaw(testContract, 'panic_test', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: explicit guest panic'
+    );
+
+    r = await ali.callRaw(testContract, 'panic_ascii_test', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: abc'
+    );
+
+    r = await ali.callRaw(testContract, 'panic_js_number', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: 356'
+    );
+
+    r = await ali.callRaw(testContract, 'panic_js_undefined', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: explicit guest panic'
+    );
+
+    r = await ali.callRaw(testContract, 'panic_js_null', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: null'
+    );
+
+    r = await ali.callRaw(testContract, 'panic_utf8_test', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: 水'
+    );
+
+    r = await ali.callRaw(testContract, 'panicUtf8_valid_utf8_sequence', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'Smart contract panicked: 水'
+    );
+
+    r = await ali.callRaw(testContract, 'panicUtf8_invalid_utf8_sequence', '');
+    t.deepEqual(
+        r.result.receipts_outcome[0].outcome.status.Failure.ActionError.kind.FunctionCallError.ExecutionError,
+        'String encoding is bad UTF-8 sequence.'
+    );
+})
