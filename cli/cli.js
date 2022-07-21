e8a0b5f807bfb755af0a66e9546d9d0aa8de10ec
Serhii Volovyk <sergeyvolovyk@gmail.com>
2022-07-22 00:48:40 +0300

    test commit

--- cli/cli.js
+++ cli/cli.js
@@ -137,7 +137,8 @@ async function createStandaloneWasmContract(qjscTarget, standaloneContractTarget
     await executeCommand(`mv ${qjscTarget} build/code.h`);
 
     await executeCommand('pwd');
-    await executeCommand('ls');
+    await executeCommand('ls node_modules/near-sdk-js/cli/deps');
+    await executeCommand('ls node_modules/near-sdk-js/cli/deps/quickjs');
     await executeCommand(`${CC} --target=wasm32-wasi -nostartfiles -Oz -flto ${DEFS} ${INCLUDES} ${SOURCES} ${LIBS} -Wl,--no-entry -Wl,--allow-undefined -Wl,-z,stack-size=${256 * 1024} -Wl,--lto-O3 -o ${standaloneContractTarget}`);
 }
 
