5609cc077f802d9d856f46f9d20c6ae0d2f915b3
Dennis <10233439+idea404@users.noreply.github.com>
2022-07-26 18:08:18 +0000

    use promises in ftTransferCall

--- examples/src/fungible-token.js
+++ examples/src/fungible-token.js
@@ -64,11 +64,11 @@ class FungibleToken extends NearContract {
     @call
     ftTransferCall({ receiverId, amount, memo, msg }) {
         let senderId = near.predecessorAccountId()
-        this.internalTransfer({ senderId, receiverId, amount, memo })
-        let onTransferRet = near.jsvmCall(receiverId, 'ftOnTransfer', { senderId, amount, msg, receiverId })
-        // In JS, do not need a callback, ftResolveTransfer after ftOnTransfer Returns
-        // If any logic after ftOnTransfer Returns is required, just do it on onTransferRet.
-        return onTransferRet
+        this.internalTransfer({ senderId, receiverId, amount, memo });
+        const promise = near.promiseBatchCreate(receiverId);
+        const params = { senderId: senderId, amount: amount, msg: msg, receiverId: receiverId };
+        near.promiseBatchActionFunctionCall(promise, 'ftOnTransfer', JSON.stringify(params), 0, 30000000000000);        
+        return near.promiseReturn();
     }
 
     @view
