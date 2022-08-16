72bcc97d66d9f71ab9fe0dc5471e37afce521022
Bo Yao <bo@near.org>
2022-08-16 15:46:49 +0800

    add missing file

---
+++ tests/__tests__/typescript.ava.js
@@ -0,0 +1,37 @@
+import { Worker } from 'near-workspaces';
+import test from 'ava';
+
+
+test.before(async t => {
+    // Init the worker and start a Sandbox server
+    const worker = await Worker.init();
+
+    // Prepare sandbox for tests, create accounts, deploy contracts, etx.
+    const root = worker.rootAccount;
+
+    // Deploy the test contract.
+    const typescriptContract = await root.devDeploy(
+        'build/typescript.wasm',
+    );
+    await typescriptContract.call(typescriptContract, 'init', {})
+
+    // Test users
+    const ali = await root.createSubAccount('ali');
+
+    // Save state for test runs
+    t.context.worker = worker;
+    t.context.accounts = { root, typescriptContract, ali };
+});
+
+test.after(async t => {
+    await t.context.worker.tearDown().catch(error => {
+        console.log('Failed to tear down the worker:', error);
+    });
+});
+
+
+test('bigint', async t => {
+    const { typescriptContract } = t.context.accounts;
+    let r = await typescriptContract.view('bigint', '');
+    t.is(r, "3");
+});
