2e9e197370f7fdca57adf7ba8e5228279d3b6a9d
Bo Yao <bo@near.org>
2022-08-16 17:30:35 +0800

    es2020 target in tsc to allow bigint literal (#176)
    * es2020 target in tsc to allow bigint literal
    
    * set module resolution for es2020 target
    
    * add a test
    
    Co-authored-by: near-bulldozer[bot] <73298989+near-bulldozer[bot]@users.noreply.github.com>

---
+++ tests/src/typescript.ts
@@ -0,0 +1,18 @@
+import {
+    NearContract,
+    NearBindgen,
+    view,
+} from 'near-sdk-js'
+
+@NearBindgen
+class TypeScriptTestContract extends NearContract {
+    @view
+    bigint() {
+        // JSON.stringify cannot seriaize a BigInt, need manually toString
+        return (1n + 2n).toString()
+    }
+
+    default() {
+        return new TypeScriptTestContract()
+    }
+}
