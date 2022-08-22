663354e4019d0298c250cf1b326da4b487fa84ae
Bo Yao <bo@near.org>
2022-08-22 19:13:00 +0800

    add all rust sdk non-deprecated types (#181)
    * add all rust sdk non-deprecated types
    * fix build by using a typescript esm base58 library
    * revert promiseResult
    * add public key tests

---
+++ lib/types/primitives.d.ts
@@ -0,0 +1,4 @@
+export declare type StorageUsage = bigint;
+export declare type BlockHeight = bigint;
+export declare type EpochHeight = bigint;
+export declare type Balance = bigint;
---
+++ lib/types/vm_types.d.ts
@@ -0,0 +1,12 @@
+export declare type PromiseIndex = bigint;
+export declare type ReceiptIndex = bigint;
+export declare type IteratorIndex = bigint;
+export declare enum PromiseResult {
+    NotReady = 0,
+    Successful = 1,
+    Failed = 2
+}
+export declare enum PromiseError {
+    Failed = 0,
+    NotReady = 1
+}
