a6e4341c7b5edeedfd549791a53238e57b1822cf
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-09-01 13:36:54 +0300

    near-bingen tests added

---
+++ tests/src/near_bindgen/require_init_true.ts
@@ -0,0 +1,36 @@
+import {
+    near,
+    NearBindgen,
+    call,
+    view,
+    initialize,
+} from 'near-sdk-js'
+
+@NearBindgen({ requireInit: true })
+class NBTest {
+    status: string;
+
+    constructor() {
+        this.status = '';
+    }
+
+    @initialize
+    init({ status }: { status: string }): void {
+        near.log(`init: ${status}`)
+        this.status = status;
+    }
+
+    @view
+    getStatus(): string {
+        near.log(`getStatus: ${this.status}`)
+        return this.status;
+    }
+
+    @call
+    setStatus({ status }: { status: string }): void {
+        near.log(`setStatus: ${status}`)
+        this.status = status;
+    }
+}
+
+
