68cf8632b87c1828a96b6108c0c0278e0731b197
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-09-06 16:50:07 +0300

    test contracts for payable and private added

---
+++ tests/src/near_bindgen/payable.ts
@@ -0,0 +1,38 @@
+import {
+    near,
+    NearBindgen,
+    call,
+    view,
+} from 'near-sdk-js'
+
+@NearBindgen({})
+class PayableTest {
+    value: string;
+
+    constructor() {
+        this.value = '';
+    }
+
+    @call({ payable: true })
+    setValueWithPayableFunction({ value }: { value: string }): void {
+        near.log(`payableFunction: ${value}`)
+        this.value = value;
+    }
+
+    @call({ payable: false })
+    setValueWithNotPayableFunction({ value }: { value: string }): void {
+        near.log(`notPayableFunction: ${value}`)
+        this.value = value;
+    }
+
+    @call({})
+    setValueWithNotPayableFunctionByDefault({ value }: { value: string }): void {
+        near.log(`notPayableFunctionDefault: ${value}`)
+        this.value = value;
+    }
+
+    @view({})
+    getValue(): string {
+        return this.value;
+    }
+}
