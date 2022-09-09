4b9d02d1aa6780637a0579e42af5c97e4699ed86
Bo Yao <bo@near.org>
2022-09-09 14:51:39 +0800

    (state breaking) make get set in unorderedmap more efficient

--- src/collections/unordered-set.ts
+++ src/collections/unordered-set.ts
@@ -34,9 +34,6 @@ export class UnorderedSet {
     return this.elements.length;
   }
 
-  // noop, called by deserialize
-  private set length(_l: number) {}
-
   isEmpty(): boolean {
     return this.elements.isEmpty();
   }
@@ -124,8 +121,6 @@ export class UnorderedSet {
     // removing readonly modifier
     type MutableUnorderedSet = Mutable<UnorderedSet>;
     let set = new UnorderedSet(data.prefix) as MutableUnorderedSet;
-    // reconstruct UnorderedSet
-    set.length = data.length;
     // reconstruct Vector
     let elementsPrefix = data.prefix + "e";
     set.elements = new Vector(elementsPrefix);
