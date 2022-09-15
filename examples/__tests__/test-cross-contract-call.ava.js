b827cebd89de76b430e44eca55639971d210a3fc
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-09-15 05:36:30 +0300

    always kill sandbox process (#220)
    * always kill sandbox process
    
    * after() -> after.always()

--- examples/__tests__/test-cross-contract-call.ava.js
+++ examples/__tests__/test-cross-contract-call.ava.js
@@ -36,7 +36,7 @@ test.beforeEach(async t => {
     };
 });
 
-test.afterEach(async t => {
+test.afterEach.always(async t => {
     await t.context.worker.tearDown().catch(error => {
         console.log('Failed tear down the worker:', error);
     });
