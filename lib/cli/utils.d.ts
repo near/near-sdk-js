04a0b6ddbc8841c855c7010ce0b78d44724c1dff
Salkanovic Omer <osalkanovic5@gmail.com>
2022-10-26 03:51:32 +0200

    fix: constructor validation on build (#226)
    * fix: constructor validation on build
    
    * Update unordered-map.js
    
    * feat: constructor checker
    
    * feat: constructor validation
    
    * fix: resolve conflicts
    
    * fix: resolve conflicts
    
    * fix: code cleaning
    
    * fix: code cleaning
    
    * fix: code cleaning
    
    * fix: lint
    
    * fix: tests
    
    * fix: empty state contract
    
    * fix: update contract valdiation to handle clean-state example
    
    * linter fix
    
    * constructor validation moved to utils
    
    * constructor test contracts renamed, typos fixed
    
    * redandunt comment deleted
    
    * Error messages changed
    
    * log comments added to contract validation
    
    * yarn build
    
    * cli/cli.js deleted
    
    * contract validation added to the main pipeline
    
    * contract validation rewrittent with TS
    
    * signal used in contract validation, tests rewritten
    
    * contract validation doc added
    
    Co-authored-by: Serhii Volovyk <SergeyVolovyk@gmail.com>

--- lib/cli/utils.d.ts
+++ lib/cli/utils.d.ts
@@ -1,2 +1,7 @@
 export declare function executeCommand(command: string, verbose?: boolean): Promise<string>;
 export declare function download(url: string, verbose?: boolean): Promise<void>;
+/**
+ * Validates the contract by checking that all parameters are initialized in the constructor. Works only for contracts written in TypeScript.
+ * @param contractPath Path to the contract.
+ **/
+export declare function validateContract(contractPath: string): Promise<boolean>;
