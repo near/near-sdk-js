286efc2813bf661c8e0a0e1a140c8207f4d6cc87
Petar Vujović <petarvujovic98@gmail.com>
2022-10-27 11:54:12 +0200

    chore: Reformat code (#274)
    Reformat code in cli and cli utils

--- lib/cli/utils.d.ts
+++ lib/cli/utils.d.ts
@@ -2,6 +2,8 @@ export declare function executeCommand(command: string, verbose?: boolean): Prom
 export declare function download(url: string, verbose?: boolean): Promise<void>;
 /**
  * Validates the contract by checking that all parameters are initialized in the constructor. Works only for contracts written in TypeScript.
- * @param contractPath Path to the contract.
+ *
+ * @param contractPath - Path to the contract.
+ * @param verbose - Whether to print verbose output.
  **/
-export declare function validateContract(contractPath: string): Promise<boolean>;
+export declare function validateContract(contractPath: string, verbose?: boolean): Promise<boolean>;
