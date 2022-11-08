e7adcc169eda65410dda63fd1a384a6938865c32
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-11-08 18:05:04 +0200

    README and main export added

--- near-contract-standards/src/index.ts
+++ near-contract-standards/src/index.ts
@@ -1,2 +1,4 @@
-/** Non-fungible tokens as described in [by the spec](https://nomicon.io/Standards/NonFungibleToken/README.html). */
+/** Non-fungible tokens as described in [by the spec](https://nomicon.io/Standards/NonFungibleToken). */
 export { NonFungibleToken } from "./non_fungible_token";
+/** Fungible tokens as described in [by the spec](https://nomicon.io/Standards/FungibleToken). */
+export { FungibleToken } from "./fungible_token";
