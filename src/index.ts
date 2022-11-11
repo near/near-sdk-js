a91bca9cf04b6d3f439c1ecbcf4155993b140001
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-11-11 17:26:58 +0200

    all primitives and utils exported in main index

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,6 @@
-import { call, view, initialize, NearBindgen } from "./near-bindgen";
-
-import * as near from "./api";
-import {
-  LookupMap,
-  Vector,
-  LookupSet,
-  UnorderedMap,
-  UnorderedSet,
-} from "./collections";
-
-import { bytes, Bytes, assert, validateAccountId, serialize } from "./utils";
-
-import { NearPromise, PromiseOrValue } from "./promise";
-
-import { AccountId } from "./types";
-
-export {
-  call,
-  view,
-  initialize,
-  NearBindgen,
-  near,
-  LookupMap,
-  Vector,
-  LookupSet,
-  UnorderedMap,
-  UnorderedSet,
-  bytes,
-  Bytes,
-  assert,
-  validateAccountId,
-  serialize,
-  NearPromise,
-  PromiseOrValue,
-  AccountId,
-};
+export * as near from "./api";
+export * from "./types";
+export * from "./near-bindgen";
+export * from "./collections";
+export * from "./utils";
+export * from "./promise";
