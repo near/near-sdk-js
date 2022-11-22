b167ce5c8fb0b0a4df3f743d87601d5bfb9ee64e
Daniyar Itegulov <ditegulov@gmail.com>
2022-11-22 19:21:24 +1100

    re-format

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
-const PACKAGE_JSON = JSON.parse(fs.readFileSync(path.join(fileURLToPath(import.meta.url), "..", "..", "package.json"), 'utf-8'));
+const PACKAGE_JSON = JSON.parse(
+  fs.readFileSync(
+    path.join(fileURLToPath(import.meta.url), "..", "..", "package.json"),
+    "utf-8"
+  )
+);
 export const LIB_VERSION: string = PACKAGE_JSON["version"];
