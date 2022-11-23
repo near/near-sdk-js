ee2a8f613391d388b3b950bdda1abb27a361d1d3
Daniyar Itegulov <ditegulov@gmail.com>
2022-11-23 22:32:13 +1100

    simplify package json parsing

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,9 @@
 import * as fs from "fs";
-import * as path from "path";
 import { fileURLToPath } from "url";
 
 const PACKAGE_JSON = JSON.parse(
   fs.readFileSync(
-    path.join(fileURLToPath(import.meta.url), "..", "..", "package.json"),
+    fileURLToPath(new URL("../package.json", import.meta.url)),
     "utf-8"
   )
 );
