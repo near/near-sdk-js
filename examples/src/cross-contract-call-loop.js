b0b0f50e3172f0b37521bb08b35b6411579a1373
Dennis <10233439+idea404@users.noreply.github.com>
2022-11-28 15:45:09 +0100

    Include Example and Test for Promises Created in For Loops (#309)
    * include example for promises created in for loops
    Co-authored-by: Serhii Volovyk <SergeyVolovyk@gmail.com>

---
+++ examples/src/cross-contract-call-loop.js
@@ -0,0 +1,61 @@
+import { call, near, NearBindgen, NearPromise, view } from "near-sdk-js";
+
+const CONTRACTS = [
+  "first-contract.test.near",
+  "second-contract.test.near",
+  "third-contract.test.near",
+];
+const NO_ARGS = "";
+const THIRTY_TGAS = BigInt("30" + "0".repeat(12));
+
+@NearBindgen({})
+class LoopXCC {
+  constructor() {
+    this.count = 0;
+  }
+
+  @call({})
+  incrementCount() {
+    let promise = NearPromise.new(CONTRACTS[0]).functionCall(
+      "getCount",
+      NO_ARGS,
+      BigInt(0),
+      THIRTY_TGAS
+    );
+    for (let i = 1; i < CONTRACTS.length; i++) {
+      promise = promise.and(
+        NearPromise.new(CONTRACTS[i]).functionCall(
+          "getCount",
+          NO_ARGS,
+          BigInt(0),
+          THIRTY_TGAS
+        )
+      );
+    }
+    promise = promise.then(
+      NearPromise.new(near.currentAccountId()).functionCall(
+        "_incrementCountCallback",
+        NO_ARGS,
+        BigInt(0),
+        THIRTY_TGAS
+      )
+    );
+    return promise.asReturn();
+  }
+
+  @call({ privateFunction: true })
+  _incrementCountCallback() {
+    const callCount = near.promiseResultsCount();
+    for (let i = 0; i < callCount; i++) {
+      const promiseResult = near.promiseResult(i);
+      const result = JSON.parse(promiseResult);
+      this.count += result;
+    }
+    return this.count;
+  }
+
+  @view({})
+  getCount() {
+    return this.count;
+  }
+}
