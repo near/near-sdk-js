a228b845e154d3867f0cce4ad447ae9f26cc953f
Serhii Volovyk <SergeyVolovyk@gmail.com>
2022-12-06 19:14:08 +0200

    nft impl exportes improved

--- packages/near-contract-standards/src/non_fungible_token/index.ts
+++ packages/near-contract-standards/src/non_fungible_token/index.ts
@@ -1,24 +1,24 @@
-/** The Token struct for the non-fungible token. */
-export { Token, TokenId } from "./token";
+/** The [approval management standard](https://nomicon.io/Standards/NonFungibleToken/ApprovalManagement.html) for NFTs. */
+export * from "./approval";
 
 /** The [core non-fungible token standard](https://nomicon.io/Standards/NonFungibleToken/Core.html). This can be though of as the base standard, with the others being extension standards. */
-export * as core from "./core";
-
-/** The [approval management standard](https://nomicon.io/Standards/NonFungibleToken/ApprovalManagement.html) for NFTs. */
-export * as approval from "./approval";
+export * from "./core";
 
 /** Interface for the [NFT enumeration standard](https://nomicon.io/Standards/NonFungibleToken/Enumeration.html).
  * This provides useful view-only methods returning token supply, tokens by owner, etc.
  */
-export * as enumeration from "./enumeration";
+export * from "./enumeration";
+
+export * from "./events";
+export * from "./impl";
 
 /** Metadata interfaces and implementation according to the [NFT enumeration standard](https://nomicon.io/Standards/NonFungibleToken/Metadata.html).
  * This covers both the contract metadata and the individual token metadata.
  */
-export * as metadata from "./metadata";
+export * from "./metadata";
+
+/** The Token struct for the non-fungible token. */
+export * from "./token";
 
 /** NFT utility functions */
 export * from "./utils";
-
-export * as events from "./events";
-export { NonFungibleToken } from "./impl";
