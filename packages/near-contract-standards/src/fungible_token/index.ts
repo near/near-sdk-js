3931fd4431fba28bc5112e996d7fa5589ec9ca4f
Serhii Volovyk <SergeyVolovyk@gmail.com>
2023-01-10 16:07:08 +0200

    exports added to ft index

---
+++ packages/near-contract-standards/src/fungible_token/index.ts
@@ -0,0 +1,7 @@
+export * from './core_impl';
+export * from './core';
+export * from './events';
+// export * from './macros';
+export * from './metadata';
+export * from './receiver';
+export * from './resolver';
