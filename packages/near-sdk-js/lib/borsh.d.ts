8ca9486f2ae44ee6580b46132dbfc3049cf47a35
Spring Chiu <fospring@outlook.com>
2023-08-06 17:10:06 +0800

    chore: update borsh dep version

--- packages/near-sdk-js/lib/borsh.d.ts
+++
@@ -1,4 +0,0 @@
-import { Schema } from "borsh";
-import { DecodeTypes } from "borsh/lib/types/types";
-export declare function borshSerialize(schema: Schema, value: unknown, checkSchema?: boolean): Uint8Array;
-export declare function borshDeserialize(schema: Schema, buffer: Uint8Array, checkSchema?: boolean): DecodeTypes;
